ca3c4c394e305523cb948cd9acbd7bfde6ea2dcc
aviadb <aviadb77@gmail.com>
2023-09-13 16:34:10 +0300

    update models and weights dictionaries

--- main.py
+++ main.py
@@ -2,27 +2,92 @@
 import torch
 import torchvision
 import torchvision.transforms as T
-from torchvision.models import resnet50, resnet101, ResNet50_Weights, ResNet101_Weights
+from torchvision.models import (
+    resnet18, resnet34, resnet50, resnet101, resnet152,
+    ResNet18_Weights, ResNet34_Weights,
+    ResNet50_Weights, ResNet101_Weights,
+    ResNet152_Weights
+)
 from torchvision.io import read_image
+import pandas as pd
 
 #%%
 
 # img = read_image("test/assets/encode_jpeg/grace_hopper_517x606.jpg")
-img = read_image("imgs/dog.jpg")
 
 # model = torch.hub.load('pytorch/vision:v0.15.2', 'resnet101', pretrained=True)
 
 # Step 1: Initialize model with the best available weights
-w_resnet50 = ResNet50_Weights.IMAGENET1K_V1
-rnet50 = resnet50(weights=w_resnet50)
-rnet50.eval();
 
-w_resnet101 = ResNet101_Weights.IMAGENET1K_V1
-rnet101 = resnet101(weights=w_resnet101)
-rnet101.eval();
+models_list = ['ResNet18', 'ResNet34', 'ResNet50', 'ResNet101', 'ResNet152']
 
-preproc_rnet50 = w_resnet50.transforms()
-preproc_rnet101 = w_resnet101.transforms()
+model = dict.fromkeys(models_list)
+weights = dict.fromkeys(models_list)
+pre_proc = dict.fromkeys(models_list)
+DS_ver = 'IMAGENET1K_V1'
+for net_model in models_list:
+    weights[net_model] = eval('{}_Weights.{}'.format(net_model, DS_ver))
+    ctor = net_model.lower()
+    model[net_model] = eval(f'{ctor}(weights={weights[net_model]})')
+    model[net_model].eval();
+    pre_proc[net_model] = weights[net_model].transforms()
+    # model[net_model] = net_model.lower()(weights=weights[net_model])
+# weights = {
+#     'resnet18': ResNet18_Weights.IMAGENET1K_V1,
+#     'resnet34': ResNet34_Weights.IMAGENET1K_V1,
+#     'resnet50': ResNet50_Weights.IMAGENET1K_V1,
+#     'resnet101': ResNet101_Weights.IMAGENET1K_V1,
+#     'resnet152': ResNet152_Weights.IMAGENET1K_V1
+# }
+# model['resnet18'] = resnet18(weights=weights['resnet18'])
+# model['resnet34'] = resnet34(weights=weights['resnet34'])
+# model['resnet50'] = resnet50(weights=weights['resnet50'])
+# model['resnet101'] = resnet101(weights=weights['resnet101'])
+# model['resnet152'] = resnet152(weights=weights['resnet152'])
+
+# for net_model in model.keys():
+#     model[net_model].eval();
+
+# w_resnet18 = ResNet18_Weights.IMAGENET1K_V1
+# rnet18 = resnet18(weights=w_resnet18)
+# rnet18.eval();
+
+# w_resnet34 = ResNet34_Weights.IMAGENET1K_V1
+# rnet34 = resnet34(weights=w_resnet34)
+# rnet34.eval();
+
+# w_resnet50 = ResNet50_Weights.IMAGENET1K_V1
+# rnet50 = resnet50(weights=w_resnet50)
+# rnet50.eval();
+
+# w_resnet101 = ResNet101_Weights.IMAGENET1K_V1
+# rnet101 = resnet101(weights=w_resnet101)
+# rnet101.eval();
+
+# w_resnet152 = ResNet152_Weights.IMAGENET1K_V1
+# rnet152 = resnet152(weights=w_resnet152)
+# rnet152.eval();
+
+# preproc_rnet18 = w_resnet18.transforms()
+# preproc_rnet34 = w_resnet34.transforms()
+# preproc_rnet50 = w_resnet50.transforms()
+# preproc_rnet101 = w_resnet101.transforms()
+# preproc_rnet152 = w_resnet152.transforms()
+
+#%%
+img = read_image("imgs/dog.jpg")
+T.ToPILImage()(img).show()
+
+batch_res18 = preproc_rnet18(img).unsqueeze(0)
+batch_res34 = preproc_rnet34(img).unsqueeze(0)
+batch_res50 = preproc_rnet50(img).unsqueeze(0)
+batch_res101 = preproc_rnet101(img).unsqueeze(0)
+batch_res152 = preproc_rnet152(img).unsqueeze(0)
+
+#%%
+prediction
+
+#%%
 
 # preprocess = weights.transforms()
 # input_tensor = preprocess(img)
@@ -49,7 +114,8 @@ preproc_rnet101 = w_resnet101.transforms()
 
 #%%
 # Step 3: Apply inference preprocessing transforms
-batch = preprocess(img).unsqueeze(0)
+batch_res50 = preproc_rnet50(img).unsqueeze(0)
+batch_res101 = preproc_rnet101(img).unsqueeze(0)
 
 # Step 4: Use the model and print the predicted category
 prediction = model(batch).detach().squeeze(0).softmax(0)

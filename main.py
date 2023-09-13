fef44a204a51deb306ffd56766ef6b4456ca71a3
aviadb <aviadb77@gmail.com>
2023-09-13 09:38:11 +0300

    split resnet50 from resnet101 for comparison

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import torch
 import torchvision
 import torchvision.transforms as T
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet50, resnet101, ResNet50_Weights, ResNet101_Weights
 from torchvision.io import read_image
 
 #%%
@@ -10,17 +10,44 @@ from torchvision.io import read_image
 # img = read_image("test/assets/encode_jpeg/grace_hopper_517x606.jpg")
 img = read_image("imgs/dog.jpg")
 
+# model = torch.hub.load('pytorch/vision:v0.15.2', 'resnet101', pretrained=True)
 
 # Step 1: Initialize model with the best available weights
-weights = ResNet50_Weights.IMAGENET1K_V1
-model = resnet50(weights=weights)
-model.eval();
+w_resnet50 = ResNet50_Weights.IMAGENET1K_V1
+rnet50 = resnet50(weights=w_resnet50)
+rnet50.eval();
+
+w_resnet101 = ResNet101_Weights.IMAGENET1K_V1
+rnet101 = resnet101(weights=w_resnet101)
+rnet101.eval();
+
+preproc_rnet50 = w_resnet50.transforms()
+preproc_rnet101 = w_resnet101.transforms()
+
+# preprocess = weights.transforms()
+# input_tensor = preprocess(img)
+# input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+
+# if torch.cuda.is_available():
+#     input_batch = input_batch.to('cuda')
+#     model.to('cuda')
+
+# with torch.no_grad():
+#     output = model(input_batch)
+# # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
+# # print(output[0])
+# # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+# probabilities = torch.nn.functional.softmax(output[0], dim=0)
+# # print(probabilities)
+
+# class_id = probabilities.argmax().item()
+# score = probabilities[class_id].item()
+# category_name = weights.meta["categories"][class_id]
+
+# print(f"{category_name}: {100 * score:.1f}%")
 
 #%%
-
-# Step 2: Initialize the inference transforms
-preprocess = weights.transforms()
-
 # Step 3: Apply inference preprocessing transforms
 batch = preprocess(img).unsqueeze(0)
 
@@ -44,8 +71,8 @@ T.ToPILImage()(img)
 
 #%%
 
-img = read_image("imgs/n02111889_7198.JPEG")
-T.ToPILImage()(img)
+img = read_image("imgs/n02111889_7198.JPEG") # Example image from training set
+T.ToPILImage()(img).show()
 
 #%%
 # Step 3: Apply inference preprocessing transforms
@@ -60,8 +87,8 @@ print(f"{category_name}: {100 * score:.1f}%")
 
 #%%
 
-img = read_image("imgs/ILSVRC2012_val_00035585.JPEG")
-T.ToPILImage()(img)
+img = read_image("imgs/ILSVRC2012_val_00035585.JPEG") # Another example from training set
+T.ToPILImage()(img).show()
 
 #%%
 # Step 3: Apply inference preprocessing transforms
